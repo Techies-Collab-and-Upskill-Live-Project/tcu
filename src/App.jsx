8cef69c711fbe5dfbad78c85b17dc755972155ac
CodewithOyinda <honeybee08186@gmail.com>
2024-05-29 21:30:07 +0100

    About pages in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './landingpage/Home';
+import About from './landingpage/About';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
       </Routes>
     </Router>
   )
