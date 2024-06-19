d923b376da29badedb8757f2999c08d7ad7ac7d1
CodewithOyinda <honeybee08186@gmail.com>
2024-06-19 20:52:47 +0100

    about page in progress

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,83 @@
-import us from './assets/image 39.svg';
+import us from './assets/Us.svg';
 import tufts from './assets/image 3.svg';
 import frame from './assets/Frame.svg';
 import chart from './assets/chart.svg';
 import vector from './assets/Vector.svg';
 import award from './assets/award.svg';
+import Button from '../../components/Button';
+import wireclay from './assets/Telephone wire clay.svg';
+import spiralclay from './assets/Spiral clay.svg';
+import clinton from './assets/Clinton.svg';
+import cynthia from './assets/Cynthia.svg';
+import deborah from './assets/Deborah.svg';
+import samuel from './assets/Samuel.svg';
+import ringclay from './assets/Star ring clay.svg';
+import ejikeme from './assets/Ejikeme.svg';
+import light from './assets/Light.svg';
+import temitayo from './assets/Temitayo.svg';
+import simon from './assets/Simon.svg';
+
 
 const About = () => {
+ 
+  const team =[
+    {
+      img:clinton,
+      name:'Clinton Michael',
+      role:'Founder & Product designer',
+      socials:''
+    },
+    {
+      img:cynthia,
+      name:'Cynthia Ejikeme',
+      role:'Project Manager & Brand commissioner',
+      socials:''
+    },
+    {
+      img:samuel,
+      name:'Samuel Ogboye',
+      role:'Software engineer & Technical Supervisor ',
+      socials:''
+    },
+    {
+      img:deborah,
+      name:'Deborah Adegoke',
+      role:'Frontend developer & Community manager',
+      socials:''
+    }
+  ]
+  
+const reviews = [
+  {
+   avatar:ejikeme,
+   name:'Cynthia Ejikeme',
+   role:'Project Manager',
+   feedback:'As one of the project managers in TCU 1,0, Working with designers and developers was a profound experience I hope to have again. We were able to seamlessly integrate a functional, user friendly platform.',
+   socials:'',
+  },
+  {
+   avatar:temitayo,
+   name:'Temitayo Lawal',
+   role:'Product Designer',
+   feedback:'While leading the design team to create the Donation Trace platform, it was an interesting experience and process. Working with the developers to bring our project to life was challenging, yet beautiful.',
+   socials:'',
+  },
+  {
+    avatar:simon,
+    name:'Simon Favour',
+    role:'Developer',
+    feedback:'Working with others, has been a highly rewarding experience. The team’s dedication to excellence, innovative approach to problem-solving, and collaborative spirit have made the project both challenging and fulfilling.',
+    socials:'',
+  },
+  {
+    avatar:light,
+    name:'Light',
+    role:'Product Manager',
+    feedback:'Collaborating with designers and developers  was an impactful experience. Through proper communication, we were able to integrate aesthetics and functionality, in creating a user-friendly platform.',
+    socials:'',
+  },
+]
+
   return (
     <div className='bg-[#121212] text-[#ffffff] w-full h-full px-[20px] md:px-[50px]'>
       <div className='flex flex-col md:flex-row gap-[20px]'>
@@ -27,20 +99,14 @@ const About = () => {
                     <p className='text-[11px] md:text-[18px]'>Interns</p>
                 </div>
             </div>
+            <Button className={"mt-[22.81px] max-lg:w-full max-lg:text-[10.8px] max-lg:py-[7.2px] lg:mt-[38px]"}>Learn More</Button>
+    
         </div>
       </div>
       <p className='text-[11px] md:text-[18px] pt-[20px] md:pt-[80px]'>We believe everyone deserves the opportunity to participate in the exciting world of technology. Our unique platform provides a collaborative learning environment for aspiring developers, designers, and other tech enthusiasts. Here, you'll gain practical experience working alongside industry professionals on real-world projects. It's not just learning the ropes, it's contributing to cutting-edge solutions while building your skillset and portfolio.</p>
       <p className='text-[11px] md:text-[18px] pt-[20px]'>At TCU, we foster a collaborative spirit. Our experienced team mentors newcomers, while fresh perspectives inject new ideas into the development process. This synergy fuels innovation and allows us to create truly groundbreaking tech solutions.</p>
-      <div className='flex flex-col items-center pt-[50px]'>
-        <p>Our Partners</p>
-        <div className='flex flex-wrap justify-center gap-[1rem] md:gap-[5rem] pt-[15px]'> 
-            {[...Array(7)].map((_, i) => (
-              <div key={i}>
-                <img src={tufts} alt='tufts'/>
-              </div>
-            ))}
-        </div>
-      </div>
+      
+      <div className='flex float-end'><img src={wireclay} alt='wireclay'/></div>
       <div className='flex flex-col items-center pt-[50px]'>
         <p>What we do</p>
         <p className='text-[20px] md:text-[25px] font-[700] text-center'>Empowering your brand with our holistic solution for growth</p>
@@ -54,7 +120,7 @@ const About = () => {
             <p className='text-[19px] md:text-[32px] font-[700]'>Gain experience to get Jobs.</p>
             <p className='text-[11px] md:text-[18px]'>Join our cohort to get certification, work experience to add to your resume, Employment possibility, Mentorship linkage.</p>
           </div>
-          <div className='w-full md:w-[400px] h-[288px] md:h-[254px] border border-[#222222]             bg-[#181818] rounded-[12px] flex flex-col justify-center items-center text-center p-[20px]'>
+          <div className='w-full md:w-[400px] h-[288px] md:h-[254px] border border-[#222222] bg-[#181818] rounded-[12px] flex flex-col justify-center items-center text-center p-[20px]'>
             <p className='text-[19px] md:text-[32px] font-[700]'>Helping startups with technological services</p>
             <p className='text-[11px] md:text-[18px]'>We have a seasoned team that cares for your startup more than you do. You won't believe it until you try.</p>
           </div>
@@ -68,11 +134,47 @@ const About = () => {
             Fill entry form
         </button>
       </div>
+      <div className='flex'><img src={spiralclay} alt='spiralclay'/></div>
+      <div className='flex flex-col items-center pt-[50px]'>
+        <p>Our Team</p>
+        <div className='flex flex-wrap justify-center gap-[1rem] md:gap-[5rem] pt-[15px]'> 
+           <div className='flex md:flex-row flex-col gap-[1rem]'>
+            {team && team.map((teams, index) => (
+            <div key={index}>
+             <img src={teams.img} alt='profile' className=' md:w-[296px] md:h-[205px] w-[343px] h-[205px] rounded-t-[12px]'/>
+            <div className='bg-[#181818] md:w-[296px] md:h-[150px] w-[343px] h-[202px] rounded-b-[12px] flex flex-col justify-center items-center'>
+               <p className='md:text-[20px] font-[700]'>{teams.name}</p>
+               <p className='md:text-[14px] font-[400]'>{teams.role}</p>
+            </div>
+            </div>
+            ))}
+            
+           </div>
+          
+        </div>
+        <img src={ringclay} alt='ringclay' className='flex float-end'/>
+      </div>
       <div className='flex flex-col items-center pt-[50px]'>
         <p>Reviews</p>
-        <p className='text-[20px] md:text-[25px]'>What people say about us</p>
+        <p className='text-[20px] md:text-[25px]'>What our interns say about us</p>
       </div>
-      <div className='flex flex-col md:flex-row gap-[2rem] mt-[30px]'>
+      <div className='grid md:grid-cols-4 grid-cols-2 gap-[1rem] items-center justify-center'>
+        {reviews && reviews.map((review, index) => (
+          <div key={index} className='border border-[#191919] w-[100px] h-[185px] md:w-[297px] md:h-[304px]'>
+            <div className='flex '>
+                <img src={review.avatar} alt='avatar'/>
+                <div>
+                  <p>{review.name}</p>
+                  <p>{review.role}</p>
+                </div>
+            </div>
+            <div className='w-[253px] h-[110px]'>
+              <p>{review.feedback}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <div className='flex flex-col md:flex-row gap-[2rem] mt-[30px]'>
           <div className='w-full md:w-[400px] h-[288px] md:h-[254px] border border-[#222222] bg-[#181818] rounded-[12px] flex flex-col justify-center items-center text-center p-[20px]'>
              <p>Second Layer is not just a service provider; we are partners in your project's success. We offer educational resources and workshops to empower project teams with the best practices and latest technologies.</p>
              <div className='flex gap-[2rem] pt-[20px]'>
@@ -121,7 +223,7 @@ const About = () => {
                 </div>
              </div>
           </div>
-      </div>
+      </div> */}
     </div>
   )
 }
