5819364ea25700c87312c86db8684c8111794aae
Emmanuel <hahyordeji@gmail.com>
2024-06-20 10:32:32 +0100

    Effect design changes on home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,10 @@ import { twMerge } from "tailwind-merge";
 
 const Button = ({ className, children, onclick }) => {
   return (
-    <button onClick={ onclick}
+    <button
+      onClick={onclick}
       className={twMerge(
-        "bg-primary-green text-white rounded-[44px] py-[12px] px-[27px] text-[18px] font-[700] leading-[24.3px] tracking-[1%]",
+        "bg-transparent border-[2px] border-primary-green text-primary-green rounded-[44px] py-[14px] px-[32px] text-[20px] font-[700] leading-[27px] tracking-[1%]",
         className
       )}
     >
