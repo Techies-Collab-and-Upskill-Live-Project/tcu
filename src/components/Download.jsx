5b344080c552bc69629e1a9f4d7f7a4564b97fbb
Emmanuel <hahyordeji@gmail.com>
2024-06-24 21:04:43 +0100

    consumed application form endpoint

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
 import { GoUpload } from "react-icons/go";
 
 const Download = () => {
+  const handleFIleUpload = (e) => {
+    const file = e.target.files;
+    console.log(file);
+  };
   return (
-    <div className='w-96 Satoshi pt-[20px]'>
-      <label >
-          <div className='flex justify-center items-center md:gap-[1rem] gap-[0.5rem] cursor-pointer border-[2px] border-[#13BA00] md:w-[168px] md:h-[51px] w-[91px] h-[32px] rounded-[12px]'>
-            <GoUpload className='text-[#13BA00] md:h-[20px] md:w-[20px] w-[14px] h-[14px]' />
-            <p className='md:text-[20px] text-[12px] font-[700] text-[#13BA00]'>Add File</p>
-          </div>
-          <input id='dropzone-file' type='file'className='hidden'/>
+    <div className="w-96 Satoshi pt-[20px]">
+      <label>
+        <div className="flex justify-center items-center md:gap-[1rem] gap-[0.5rem] cursor-pointer border-[2px] border-[#13BA00] md:w-[168px] md:h-[51px] w-[91px] h-[32px] rounded-[12px]">
+          <GoUpload className="text-[#13BA00] md:h-[20px] md:w-[20px] w-[14px] h-[14px]" />
+          <p className="md:text-[20px] text-[12px] font-[700] text-[#13BA00]">
+            Add File
+          </p>
+        </div>
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleFIleUpload}
+        />
       </label>
-      <p className='md:text-[20px] text-[12px] font-[400] text-[#4D4D4D] pt-[20px]'>.Jpeg, .png, .pdf, .docx</p>
+      <p className="md:text-[20px] text-[12px] font-[400] text-[#4D4D4D] pt-[20px]">
+        .Jpeg, .png, .pdf, .docx
+      </p>
     </div>
   );
-}
+};
 
 export default Download;
