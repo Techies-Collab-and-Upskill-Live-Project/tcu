c3f1a7c206f5702fa1012e49e4de5abc80335586
Theecypher <obioramma12@gmail.com>
2025-03-16 18:20:11 +0100

    updated the portfolio section

--- src/pages/portfolio/PortfolioData.jsx
+++ src/pages/portfolio/PortfolioData.jsx
@@ -1,82 +1,101 @@
-import Digital from "./Assets/DigitalProductivity.png"
-import Elearning from "./Assets/e-learning.png"
-import Social from "./Assets/Socialnetwork.png"
-import FinancialManagement from "./Assets/FinancialTool.png"
-import Charity from "./Assets/Charity.png"
-import Healthandfitness from "./Assets/HealthBenefit1.png"
-
+import Digital from "./Assets/DigitalProductivity.png";
+import Elearning from "./Assets/e-learning.png";
+import Social from "./Assets/Socialnetwork.png";
+import FinancialManagement from "./Assets/FinancialTool.png";
+import Charity from "./Assets/Charity.png";
+import Healthandfitness from "./Assets/HealthBenefit1.png";
+import FYSI from "../../assets/FYSI.jpg"
+import TAILORA from "../../assets/TAILORA.jpg"
+import ECORIDE from "../../assets/ECORIDE.jpg"
 
 export const DesignData = [
-    {
-        img: Digital,
-        title: "TimeSync",
-        description:
-          "Delivers in-depth analytics, enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
-          to: "https://www.behance.net/gallery/190639579/Timesync",
-          alt: "Digital Productivity Tool"
-      },
-      {
-        img: Elearning,
-        title: "E-learning Platform",
-        description:
-          "An educational learning platform that addresses the unidirectional learning model, by Introducing a mentorship feature.",
-          to: "https://www.behance.net/gallery/190653037/EDUVERSE-A-UIUX-CASE-STUDY",
-          alt: "E-learning Platform"
-      },
-      {
-        img: Healthandfitness,
-        title: "Health and fitness Tool",
-        description:
-          "Enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
-          to: "",
-          alt: "Health and Fitness Tool"
-      },
-      {
-        img: Social,
-        title: "Social Network Platform",
-        description: "Description of your work",
-        to: "",
-        alt: "Social Network Platform"
-      },
-      {
-        img: FinancialManagement,
-        title: "Finacial Management",
-        description: "Description of your work",
-        to: "https://www.behance.net/gallery/197500939/Nestify",
-        alt: "Financial Managemet Tool"
-      },
-      {
-        img: Charity,
-        title: "Charity/Non-profit Platform",
-        description: "Description of your work",
-        to: "",
-        alt: "Digital Picture"
-      }
-]
+  {
+    img: Digital,
+    title: "TimeSync",
+    description:
+      "Delivers in-depth analytics, enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
+    to: "https://www.behance.net/gallery/190639579/Timesync",
+    alt: "Digital Productivity Tool",
+  },
+  {
+    img: Elearning,
+    title: "E-learning Platform",
+    description:
+      "An educational learning platform that addresses the unidirectional learning model, by Introducing a mentorship feature.",
+    to: "https://www.behance.net/gallery/190653037/EDUVERSE-A-UIUX-CASE-STUDY",
+    alt: "E-learning Platform",
+  },
+  {
+    img: Healthandfitness,
+    title: "Health and fitness Tool",
+    description:
+      "Enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
+    to: "",
+    alt: "Health and Fitness Tool",
+  },
+  {
+    img: Social,
+    title: "Social Network Platform",
+    description: "Description of your work",
+    to: "",
+    alt: "Social Network Platform",
+  },
+  {
+    img: FinancialManagement,
+    title: "Finacial Management",
+    description: "Description of your work",
+    to: "https://www.behance.net/gallery/197500939/Nestify",
+    alt: "Financial Managemet Tool",
+  },
+  {
+    img: Charity,
+    title: "Charity/Non-profit Platform",
+    description: "Description of your work",
+    to: "",
+    alt: "Digital Picture",
+  },
+  {
+    img: FYSI,
+    title: "E-commerce",
+    to: "https://www.behance.net/gallery/211974639/An-Eco-Friendly-E-commerce-Platform",
+  },
+  {
+    img: TAILORA,
+    title: "The ultimate Platform for fashion designers",
+    to: "https://www.behance.net/gallery/211866631/Tailora-The-Ultimate-Platform-For-Fashion-Designers",
+    alt: "A tool for fashion designers",
+  },
+  {
+    img: ECORIDE,
+    title: "Electric Vehicle Halting Application",
+    to: "https://www.behance.net/gallery/212688849/Ecoride-An-Electric-Vehicle-Hailing-Application",
+    alt: "Eco-friendly app",
+  },
+];
 
 export const DevelopmentData = [
-    {
-        img: Digital,
-        title: "Digital Productivity Tool",
-        description:
-          "Delivers in-depth analytics, enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
-          to: "https://www.behance.net/gallery/190639579/Timesync",
-          alt: "Digital Productivity Tool"
-      },
-      {
-        img: Elearning,
-        title: "E-learning Platform",
-        description:
-          "An educational learning platform that addresses the unidirectional learning model, by Introducing a mentorship feature.",
-          to: "https://www.behance.net/gallery/190653037/EDUVERSE-A-UIUX-CASE-STUDY",
-          alt: "E-learning platform"
-      },
-      {
-        img: Healthandfitness,
-        title: "Health and fitness Tool",
-        description:
-          "Enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
-          to: "",
-          alt: "Health and Fitness Tool"
-      },
-]
+  {
+    img: Digital,
+    title: "Digital Productivity Tool",
+    description:
+      "Delivers in-depth analytics, enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
+    to: "https://www.behance.net/gallery/190639579/Timesync",
+    alt: "Digital Productivity Tool",
+  },
+  {
+    img: Elearning,
+    title: "E-learning Platform",
+    description:
+      "An educational learning platform that addresses the unidirectional learning model, by Introducing a mentorship feature.",
+    to: "https://www.behance.net/gallery/190653037/EDUVERSE-A-UIUX-CASE-STUDY",
+    alt: "E-learning platform",
+  },
+  {
+    img: Healthandfitness,
+    title: "Health and fitness Tool",
+    description:
+      "Enabling you to detect trends, monitor efficiency, and modify your plans for victory.",
+    to: "",
+    alt: "Health and Fitness Tool",
+  },
+];
